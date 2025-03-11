39b592b04189cc5cd1ccb9a2e984bfbfae1d1c59
Ben Resnick <ben.resnick@kellogg.northwestern.edu>
2025-03-11 15:07:50 +0000

    bootstrap nav bar and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "places", :action => "index" })
+  root "places#index"
   resources "entries"
   resources "places"
   resources "sessions"
   resources "users"
+
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  get "signup", to: "users#new"
 end
